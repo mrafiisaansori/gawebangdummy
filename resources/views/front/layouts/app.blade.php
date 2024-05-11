e0bdf4680ff9e94d6676c9a1ea771a1dbd319e95
mrafiisaansori <isarafi13@gmail.com>
2024-05-11 15:43:53 +0700

    hotfix: seo

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -5,6 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="{{ asset('css/output.css') }}" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+
+  <meta name="twitter:site" content="@raftech" />
+  <meta name="description" content="raftech menyediakan jasa pembuatan aplikasi">
+  <meta name="keywords" content="Web Development, Data Science, Freelancer, Raftech">
+  <meta name="author" content="raftech" />
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="Raftech">
+  <meta property="og:site_name" content="raftech">
+  <meta property="og:url" content="https://www.raftech.tech/">
+  <meta property="og:description" content="raftech menyediakan jasa pembuatan aplikasi">
 </head>
 
     @yield('content')
